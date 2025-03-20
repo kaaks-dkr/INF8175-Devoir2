4d340201db09e4ce79d0c02dab85111b0acf9265
Krumskys <quentin.luquet-de-saint-germain@polymtl.ca>
2025-03-19 23:30:46 -0400

    ajoute 1 exo

--- Recherche locale/code/solver_advanced.py
+++ Recherche locale/code/solver_advanced.py
@@ -1,147 +1,176 @@
-#KARAKI Ali 2402594
-
 from schedule import Schedule
-from random import choice
-from math import exp
+import random
+import math
+import solver_naive
 
-""" Ce problème correspond au problème connu de coloriage de graphe pour lequel on ne connaît pas d'algorithme
-    capable de le résoudre en temps polynômial.
-    
-    Il existe plusieurs approches pour résoudre ce problème comme vu dans le cours INF8775
-    
-    L'approche que l'on utilise est de commencer par colorier notre graphe avec une approche vorace, puis essayer
-    de faire des améliorations locales dans des voisinages intéressants.
-    
-    Nos candidats pour l'approche vorace sont les sommets ayant le plus grand ndegré de saturation
-    On s'inspire de l'algorithme D-satur pour implémenter cette approche gloutonne dans un premier temps
-"""
-
-def solve(schedule: Schedule):
+def solve(schedule):
     """
-    Your solution of the problem
+
     :param schedule: object describing the input
     :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
     """
-    # Add here your agent
-    solution_base = greedy(schedule)
-    solution_improved = local_improvements(schedule, solution_base)
-    return solution_improved
 
-
-"""
-Cette fonction essaie de trouver des améliorations locales pour une solution courante
-Elle utilise le simulated anealing vu en cours pour sélectionner des solutions permettant d'échapper à des minima locaux
-"""
-def local_improvements(schedule: Schedule, initial_solution, temperature = 0.9, cooling_rate = 0.991, it_number = 30000):
-    solution = initial_solution
-    for _ in range(it_number):
-        #choisir autrement que aléatoirement?
-        new_node = choice(list(solution.keys()))
-        neighbour_colors = set()
-        for neighbour in schedule.get_node_conflicts(new_node):
-            if(neighbour in solution):
-                neighbour_colors.add(solution[neighbour])
-
-        old_color = solution[new_node]
-        new_color = find_smallest(list(neighbour_colors))
-        old_score = eval(solution, schedule)
-        solution[new_node] = new_color
-        new_score = eval(solution, schedule)
-        
-        
-        if(new_score == -1):
-            solution[new_node] = old_color
-        else:
-            if(new_score > old_score):
-                prob = exp((old_score - new_score)/temperature)
-                solution[new_node] = choice([new_color, old_color], [prob, 1-prob])
-        temperature *= cooling_rate
-    return solution
-
-"""
-Cette fonction vérifie si une solution est valide ou non
-Elle est tirée de la classe schedule mais implémentée ici pour éviter l'instruction assert 
-"""
-def verify_solution(solution, schedule: Schedule):
-    return sum(solution[a[0]] == solution[a[1]] for a in schedule.conflict_list) == 0
-
-"""
-Cette fonction est une fonction d'évaluation de la solution courante
-"""
-def eval(solution, schedule: Schedule) -> int:
-    if(not verify_solution(solution, schedule)):
-        return -1
-    return schedule.get_n_creneaux(solution)
-
-
-"""
-Cette fonction retourne une solution gloutonne au problème de coloriage de graphe
-Elle utilise l'algorithme D-SATUR vu en cours de INF8775 afin de résoudre le problème
-
-Source: wikipedia: D-SATUR
-Ordonner les sommets par ordre décroissant de degrés.
-Colorer un sommet de degré maximum avec la couleur 1.
-Choisir un sommet avec DSAT maximum. En cas d'égalité, choisir un sommet de degré maximal.
-Colorer ce sommet avec la plus petite couleur possible.
-Si tous les sommets sont colorés alors stop. Sinon aller en 3.
-"""
-def greedy(schedule: Schedule):
+    # Cout (nombre de creneau + une constant fois le nombre de conflit)  
+    def cost(solution):
+        const_conflicts = 10
+        nb_conflicts = sum(
+            1 for course in schedule.course_list
+                for conflict in schedule.get_node_conflicts(course)
+                    if solution[course] == solution.get(conflict, -1)
+        )
+        cost = schedule.get_n_creneaux(solution) + const_conflicts * nb_conflicts
+        return cost
     
-    """On commence par trier les nodes dans l'ordre décroissant de leur degré"""
-    nodes_sorted = sorted(schedule.conflict_graph.degree, key=lambda x: x[1], reverse=True)
-    nodes = [nodes_sorted[i][0] for i in range(len(nodes_sorted))]
-    
-    solution = dict()
-    
-    colors = {}
-    colors[nodes[0]] = 1
-    nodes = nodes[1:]
-    
-    while nodes:
-        dsat_nodes = {}
-        for node in nodes:
-            dsat_nodes[node] = dsat(node, schedule, colors)
-        
-        target_node = max(dsat_nodes, key=dsat_nodes.get)
-        neighbor_colors = []
-        for neighbor in schedule.get_node_conflicts(target_node):
-            if(neighbor in colors):
-                if(colors[neighbor] not in neighbor_colors):
-                    neighbor_colors.append(colors[neighbor])
-        if not neighbor_colors:
-            colors[target_node] = 1
-        else:
-            colors[target_node] = find_smallest(neighbor_colors)
-        nodes.remove(target_node)
-        
-    for node in schedule.course_list:
-        solution[node] = colors[node]
-    
-    return solution
+    # Fonction de voisinage
+    def fonction_voisinage(solution):
 
-"""
-Cette fonction trouve la plus petite couleur qu'il est possible de donner à un sommet ayant des voisins
-de couleurs différentes
-"""
-def find_smallest(neighbours : list[int]) -> int:
-    neighbours.sort()
-    color = 1
-    for neighbour in neighbours:
-        if(neighbour > color):
-            return color
-        color +=1
-    return color
+        course = random.choice(list(schedule.course_list))
+        last_creaneau = max(solution.values())
+        new_creaneau = random.randint(1, last_creaneau)
+
+        return course, new_creaneau
+    
+    # Solution aleatoire 
+    def random_solution(withConflicts = False):
+        # Gerneation d'une solution aleatoire 
+        solution = dict()
+
+        # Liste des cours à assigner
+        nodes = list(schedule.course_list)
+
+        # Pour chaque cours, assigner un créneau horaire aléatoire
+        for course in nodes:
+            # On génère un créneau aléatoire, entre 1 et le nombre de cours
+            creneau = random.randint(1, len(nodes))
+
+            if not withConflicts :
+                # Tant que le créneau est déjà utilisé par un cours en conflit, on génère un nouveau créneau
+                conflicts = schedule.get_node_conflicts(course)
+                while any(solution.get(conflict) == creneau for conflict in conflicts if conflict in solution):
+                    creneau = random.randint(1, len(nodes))
+
+            solution[course] = creneau
+
+        return solution
+
+    # Solution greedy
+    def greedy():
+        # Gerneation du solution greedy 
+        gready_solution = dict()
+        nodes = list(schedule.course_list)
         
-"""
-Cette fonction est l'implémentation de la fonction Dsat:
-DSAT(v)= nombre de couleurs différentes dans les sommets adjacents à v 
-source: Wikipedia D-SATUR
-"""
-def dsat(node, schedule: Schedule, colors) -> int:
-    colors_tested = []
-    neighbors = schedule.get_node_conflicts(node)
-    for neighbor in neighbors:
-        if neighbor in colors:
-            if colors[neighbor] not in colors_tested:
-                colors_tested.append(colors[neighbor])
-    return len(colors_tested)
+        # Trie décroissant des cours avec le plus de conflits
+        for i in range(len(nodes)):
+            for j in range(i + 1, len(nodes)):
+                # Si plus de conflit
+                if schedule.conflict_graph.degree(nodes[i]) < schedule.conflict_graph.degree(nodes[j]):
+                    nodes[i], nodes[j] = nodes[j], nodes[i]
+    
+        for course in nodes:
+            # Pour chaque cours on recuper les cours en conflits avec celui-ci
+            creneau_voisin = set()
+            conflicts = schedule.get_node_conflicts(course)
+            # print("Courses is :", course, "and conflicts ", conflicts)
+            # On parcours tout les conflits pour savoir si on a deja assigner des cours ne conflits a la solution.
+            # Si on la deja assigner cela veut dire qu'on ne peux utiliser ce creneau pour ce cours
+            for c in conflicts:
+                if c in gready_solution:  
+                    creneau_voisin.add(gready_solution[c]) 
+
+            #  print("     Creneau voisn is ", creneau_voisin)
+
+            # On assigne le premier trou de creneau
+            creneau = 1
+            while creneau in creneau_voisin:
+                creneau += 1
+            gready_solution[course] = creneau
+        return gready_solution
+    
+    def simulatedAnnealing(solution, t0=1, t_min=1e-4, alpha=0.995, max_iter=100000, max_no_improve=5000):
+        """
+        :param solution: initial solution
+        :param t0:  Temperature initial
+        :param t_min: Temperature Min
+        :param alpha: Taux de decroissance (entre 0.8 et 0.99)
+        :param max_iter: Max iterations
+        :param max_no_improve: max d'itérations sans amélioration
+        :return: the best solution found
+        """
+        no_improve_count = 0   # La solution ne s'améliore pas pendant max_no_improve itérations, on arrête
+
+        current_solution = solution
+        best_solution = solution.copy()
+        # Cout Initial 
+        current_cost = cost(current_solution)
+        best_cost = current_cost
+
+        t = t0
+        iter = 0
+
+        while t > t_min and iter < max_iter:
+            iter += 1
+            course, new_creaneau = fonction_voisinage(current_solution)
+            # Pas de changement ==> next
+            if new_creaneau == current_solution[course]:
+                continue  
+            
+            # Creation d'une nouvelle solution
+            new_solution = current_solution.copy() 
+            new_solution[course] = new_creaneau
+            
+            # Calcule du cout de cette nouvelle solution
+            new_cost = cost(new_solution)
+            delta = new_cost - current_cost
+
+            # Sélection en cas de non-dégradation ou dégradation avec une certaine probabilité
+            if delta < 0 or random.random() < math.exp(-delta / t):
+                current_solution = new_solution
+                current_cost = new_cost
+                # Si on a la meilleur solution on la garde
+                if current_cost < best_cost:
+                    best_solution = new_solution.copy()
+                    best_cost = current_cost
+                else:
+                    no_improve_count += 1
+            else:
+                no_improve_count += 1
+
+            # Arrêt anticipé si aucune amélioration 
+            if no_improve_count >= max_no_improve:
+                break
+
+            # Mise a jour de la temperature
+            t = alpha * t
+        return best_solution, best_cost
+    
+    # Local search avec restart et simulatedAnnealing
+    def local_restart_simulated(max_iteration_restart = 50): 
+        best_solution_restart = random_solution(withConflicts=True)
+        best_cost_restart = cost(best_solution_restart)
+
+        for _ in range(max_iteration_restart):
+            # generate first Solution 
+            first_solution = random_solution(withConflicts=True)
+            # executer un recherche par recuit simulé 
+            solution, simul_cost = simulatedAnnealing(first_solution)
+
+            if simul_cost < best_cost_restart:
+                best_solution_restart = solution.copy()
+                best_cost_restart = simul_cost
+        return best_solution_restart, best_cost_restart
+
+    #Test avec greedy et simulatedAnnealing
+    solution_greedy = greedy()
+    solution_greedy, greedy_cost = simulatedAnnealing(solution_greedy)
+
+    # Test avec restart et simulatedAnnealing
+    solution_restart, restart_cost = local_restart_simulated()
+
+    #La meilleur solution reste avec le programme gready.
+    if greedy_cost <= restart_cost:
+        print("Greedy is better (with a little help of simulated Annealing)")
+        return solution_greedy
+    else:
+        print("It's a win for Local search Algo")
+        return solution_restart
+    
